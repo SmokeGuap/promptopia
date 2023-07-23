3ef22d19be45c7cbab504e69ed66026a962cf1cb
Kirill Ryzhkov <mustang.horse34@gmail.com>
2023-07-23 18:16:19 +0700

    Add connect to mongo and sign in

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Provider from '@/components/Provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,13 +14,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
-          <Navbar />
-          {children}
-        </main>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+          <main className='relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
+            <Navbar />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
